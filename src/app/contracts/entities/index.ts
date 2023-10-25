925cad06cad5e8e8618f6a750ca2db3c254c8568
Barfi <barfider@gmail.com>
2023-10-25 16:57:55 +0300

    Add Entities entry point

---
+++ src/app/contracts/entities/index.ts
@@ -0,0 +1,4 @@
+export { type BaseEntity, baseEntityKeys } from './BaseEntity'
+export { type CardEntity, cardEntityKeys } from './CardEntity'
+export { type TagEntity, tagEntityKeys } from './TagEntity'
+export { type CardTagsEntity, cardTagsEntityKeys } from './CardTagsEntity'
