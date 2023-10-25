072be30bd0db7e0b7295a835c498ecd6a507288f
Barfi <barfider@gmail.com>
2023-10-25 15:33:31 +0300

    Add result types

---
+++ src/app/contracts/shared/Result.ts
@@ -0,0 +1,45 @@
+/**
+ * Represents an abstract successful result
+ */
+export type SuccessfullResult<T> = {
+  isFailure: false
+  value: T
+}
+
+/**
+ * Represents an abstract failurefull result
+ */
+export type FailurefullResult<T> = {
+  isFailure: true
+  reason: T
+}
+
+/**
+ * Represents an abstract syncronous result
+ */
+export type SyncResult<T, E = Error> = SuccessfullResult<T> | FailurefullResult<E>
+
+/**
+ * Represents an abstract asyncronous result
+ */
+export type AsyncResult<T, E = Error> = Promise<SyncResult<T, E>>
+
+/**
+ * Creates successfull result with the given value
+ */
+export const success = <T>(value: T): SuccessfullResult<T> => {
+  return {
+    isFailure: false,
+    value
+  }
+}
+
+/**
+ * Creates failurefull result with the given reason
+ */
+export const failure = <T>(reason: T): FailurefullResult<T> => {
+  return {
+    isFailure: true,
+    reason
+  }
+}
