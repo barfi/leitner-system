58023f892fdb256215e584098046e887cb2a51f3
Barfi <barfider@gmail.com>
2023-10-25 15:19:46 +0300

    Add TODO for branded primitives

--- src/app/contracts/shared/BrandedPrimitives.ts
+++ src/app/contracts/shared/BrandedPrimitives.ts
@@ -7,6 +7,10 @@
  */
 export type UUID = string & { readonly __brand: unique symbol }
 
+// TODO: Add typecasts function `toUUID()`
+// TODO: Add type guard function `isUUID()`
+// https://javascript.plainenglish.io/writing-robust-typescript-with-branded-primitives-bde7cd2d1c77
+
 /**
  * Represents ISO 8601 date string
  * 
@@ -15,6 +19,10 @@ export type UUID = string & { readonly __brand: unique symbol }
  * new Date().toISOString() // => 2023-10-16T19:08:58.762Z
  */
 export type ISO8601 = string & { readonly __brand: unique symbol }
+
+// TODO: Add typecasts function `toISO8601()`
+// TODO: Add type guard function `isISO8601()`
+// https://javascript.plainenglish.io/writing-robust-typescript-with-branded-primitives-bde7cd2d1c77
 
 /**
  * Represents HEX color string
@@ -25,6 +33,10 @@ export type ISO8601 = string & { readonly __brand: unique symbol }
  */
 export type HEXColor = string & { readonly __brand: unique symbol }
 
+// TODO: Add typecasts function `toHEXColor()`
+// TODO: Add type guard function `isHEXColor()`
+// https://javascript.plainenglish.io/writing-robust-typescript-with-branded-primitives-bde7cd2d1c77
+
 /**
  * Represents score point
  * 
@@ -33,3 +45,7 @@ export type HEXColor = string & { readonly __brand: unique symbol }
  * const score: ScorePoint[] = [0, 0.25, 0.5, 0.75, 1]
  */
 export type ScorePoint = number & { readonly __brand: unique symbol }
+
+// TODO: Add typecasts function `ScorePoint()`
+// TODO: Add type guard function `ScorePoint()`
+// https://javascript.plainenglish.io/writing-robust-typescript-with-branded-primitives-bde7cd2d1c77
